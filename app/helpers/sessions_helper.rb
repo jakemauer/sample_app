94d555337e8b3e86f5e5e39e4783282b40fc7a83
Jake Mauer <jake@jakemauer.com>
2012-12-23 17:34:36 -0600

    completed ch 8

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
-    current_user = user
+    self.current_user = user
 
     ### Alternative method: 
     # cookies[:remember_token] = { value: user.remember_token, 
@@ -23,7 +23,7 @@ module SessionsHelper
   end
 
   def sign_out
-    current_user = nil
+    self.current_user = nil
     cookies.delete(:remember_token)    
   end
 
