365515a5d220e225af3f500acda2730cceed33ed
Jake Mauer <jake@jakemauer.com>
2012-12-24 00:23:54 -0600

    finished ch. 9 - Updating users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,9 +22,22 @@ module SessionsHelper
     @current_user ||= cookies[:remember_token] && User.find_by_remember_token(cookies[:remember_token])
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)    
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
   end
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,14 @@
 FactoryGirl.define do
   factory :user do
     # The following are all methods 
-    # so it's could be written as name() and email() etc.
-    name      "Jake Mauer"
-    email     "jake@jakemauer.com"
+    # so it could be written as name() and email() etc.
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password  "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
