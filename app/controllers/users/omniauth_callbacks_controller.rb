4a7d8f8b2a5a574492575d96a28122db846d5525
Jake Mauer <jake@jakemauer.com>
2012-12-24 22:26:48 -0600

    Got basic openID auth with appdirect working.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,11 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_filter :verify_authenticity_token, :only => [:google]
+  skip_before_filter :verify_authenticity_token, :only => [:open_id]
 
-  def google
+  def open_id
     @user = User.find_for_open_id(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "OpenID"
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
